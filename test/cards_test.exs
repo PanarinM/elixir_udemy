67ad69b7e438b6628dde1a53ed32ceeb0f0d895e
mpanarin <m.panarin@mobilunity.com>
2018-11-02 17:51:31 +0200

    add some tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,4 +2,14 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  test "create_deck creates 20 cards" do
+    deck_len = length(Cards.create_deck)
+    assert deck_len == 20
+  end
+
+  test "shuffle is working" do
+    deck = Cards.create_deck
+    assert length(deck) == length(Cards.shuffle(deck))
+    refute deck == Cards.shuffle(deck)
+  end
 end
