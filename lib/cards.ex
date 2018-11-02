3a9ef28406c62f18ddf4855ed1de20d519cfeaef
mpanarin <m.panarin@mobilunity.com>
2018-11-02 16:12:49 +0200

    remove bad way

--- lib/cards.ex
+++ lib/cards.ex
@@ -10,15 +10,6 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suites = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    # BAD way!
-    # cards = for value <- values do
-    #   for suit <- suites do
-    #     "#{value} of #{suit}"
-    #   end
-    # end
-    # List.flatten cards
-
-    # GOOD way!
     for suit <- suites, value <- values do
       "#{value} of #{suit}"
     end
