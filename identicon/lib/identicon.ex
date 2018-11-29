604a8482508897d132d30baecdad36a4fba630ab
mpanarin <m.panarin@mobilunity.com>
2018-11-29 20:52:49 +0200

    add grid creation

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,17 +1,43 @@
 defmodule Identicon do
   def main do
-    String.trim(IO.gets "String to use: ")
+    String.trim(IO.gets("String to use: "))
     |> hash_input
     |> get_color
+    |> build_grid
+    |> filter_grid
+  end
+
+  def filter_grid(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+    %Identicon.Image{image | grid: grid}
   end
 
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
     %Identicon.Image{hex: hex}
   end
 
   def get_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    grid =
+      hex_list
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 end
