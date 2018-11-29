6cbefb5e030e2ce16c4aed5fd8c7a1a7e25e8b16
mpanarin <m.panarin@mobilunity.com>
2018-11-29 21:55:45 +0200

    add docs, doctests

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,4 +1,12 @@
 defmodule Identicon do
+  @moduledoc """
+    A module for creating identicon images
+  """
+
+  @doc """
+    Modules entrypoint, asks for an input and then generates an image
+    with name *imput*.png
+  """
   def main do
     input = String.trim(IO.gets("String to use: "))
     input
@@ -26,6 +34,19 @@ defmodule Identicon do
     :egd.render(image)
   end
 
+  @doc """
+  Creates a pixel map for a grid of a struct. Made for a 250x250 (50x50) image
+
+  ## Example
+
+  iex> Identicon.build_pixel_map(%Identicon.Image{grid: [{2, 1}, {2, 3}]})
+  %Identicon.Image{
+    color: nil,
+    grid: [{2, 1}, {2, 3}],
+    hex: nil,
+    pixel_map: [{{50, 0}, {100, 50}}, {{150, 0}, {200, 50}}]
+  }
+ """
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map = Enum.map grid, fn({_code, index}) ->
       horizontal = rem(index, 5) * 50
@@ -37,6 +58,19 @@ defmodule Identicon do
     %Identicon.Image{image | pixel_map: pixel_map}
   end
 
+  @doc """
+  Filters the grid value of a struct, so only rows with even values are left
+
+  ## Example
+
+      iex> Identicon.filter_grid(%Identicon.Image{grid: [{1, 0}, {2, 1}, {3, 2}, {2, 3}]})
+      %Identicon.Image{
+        color: nil,
+        grid: [{2, 1}, {2, 3}],
+        hex: nil,
+        pixel_map: nil
+      }
+  """
   def filter_grid(%Identicon.Image{grid: grid} = image) do
     grid = Enum.filter grid, fn({code, _index}) ->
       rem(code, 2) == 0
@@ -44,6 +78,20 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
+  @doc """
+  Hashes the input, hash will further be used for image generation.
+  returns an Identicon.Image struct with hex value filled
+
+  ## Example
+
+      iex> Identicon.hash_input("asdf")
+      %Identicon.Image{
+         color: nil,
+         grid: nil,
+         hex: [145, 46, 200, 3, 178, 206, 73, 228, 165, 65, 6, 141, 73, 90, 181, 112],
+         pixel_map: nil
+      }
+  """
   def hash_input(input) do
     hex =
       :crypto.hash(:md5, input)
@@ -52,10 +100,38 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
+  @doc """
+  Method for obtaining the color from the hex of a struct.
+  Returns a new struct with a color key filled.
+
+  ## Example
+
+      iex> Identicon.get_color(%Identicon.Image{hex: [1, 2, 3, 4, 5, 6]})
+      %Identicon.Image{
+         color: {1, 2, 3},
+         grid: nil,
+         hex: [1, 2, 3, 4, 5, 6],
+         pixel_map: nil
+      }
+  """
   def get_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
+  @doc """
+  Creates a grid for the identicon to be rendered from
+  Returns a struct with filled grid parameter
+
+  ## Example
+
+      iex> Identicon.build_grid(%Identicon.Image{hex: [1, 2, 3, 4, 5, 6]})
+      %Identicon.Image{
+         color: nil,
+         grid: [{1, 0}, {2, 1}, {3, 2}, {2, 3}, {1, 4}, {4, 5}, {5, 6}, {6, 7}, {5, 8}, {4, 9}],
+         hex: [1, 2, 3, 4, 5, 6],
+         pixel_map: nil
+      }
+  """
   def build_grid(%Identicon.Image{hex: hex_list} = image) do
     grid =
       hex_list
@@ -66,6 +142,14 @@ defmodule Identicon do
     %Identicon.Image{image | grid: grid}
   end
 
+  @doc """
+  Mirrors the row of 3 elements
+
+  ## Example
+
+      iex> Identicon.mirror_row([1, 2, 3])
+      [1, 2, 3, 2, 1]
+  """
   def mirror_row(row) do
     [first, second | _tail] = row
     row ++ [second, first]
