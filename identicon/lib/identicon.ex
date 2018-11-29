e527c50917ae88bf12606505c3c6f4cec70970fc
mpanarin <m.panarin@mobilunity.com>
2018-11-29 21:23:56 +0200

    finish the identicon project

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,10 +1,40 @@
 defmodule Identicon do
   def main do
-    String.trim(IO.gets("String to use: "))
+    input = String.trim(IO.gets("String to use: "))
+    input
     |> hash_input
     |> get_color
     |> build_grid
     |> filter_grid
+    |> build_pixel_map
+    |> generate_image
+    |> save_image(input)
+  end
+
+  def save_image(image_binary, filename) do
+    File.write("#{filename}.png", image_binary)
+  end
+
+  def generate_image(%Identicon.Image{pixel_map: pixel_map, color: color}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+      {top_left, bottom_right}
+    end
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_grid(%Identicon.Image{grid: grid} = image) do
